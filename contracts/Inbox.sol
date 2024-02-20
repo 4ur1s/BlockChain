75f9bd748d98344b83cb02c843a0f1cbad4d6eb9
4ur1s <raphaelauris@gmail.com>
2024-02-20 19:12:04 -0300

    Comienza

---
+++ contracts/Inbox.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.24;
+
+// SPDX-License-Identifier: MIT
+
+contract Inbox {
+    string public message;
+
+    constructor(string memory initialMessage) {
+        message = initialMessage;
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+}
